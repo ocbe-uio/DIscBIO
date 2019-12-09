7c8493e3a774b30a1a3345f223172cce789c8a3a
Waldir Leoncio <waldir.leoncio@gmail.com>
2019-12-09 15:49:05 +0100

    Added datasets

--- R/datasets.R
+++ R/datasets.R
@@ -3,3 +3,15 @@
 #' @name valuesG1ms
 #' @docType data
 NULL
+
+#' MLSrawWithoutERCC data
+#'
+#' @name MLSrawWithoutERCC
+#' @docType data
+NULL
+
+#' GeneNames data
+#'
+#' @name GeneNames
+#' @docType data
+NULL
