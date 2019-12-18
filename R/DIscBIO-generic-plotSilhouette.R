6e1e0690810103b2bcefd701694d91d70b86eed1
Waldir Leoncio <waldir.leoncio@gmail.com>
2019-12-18 16:07:35 +0100

    Improved formatting and argument passing

--- R/DIscBIO-generic-plotSilhouette.R
+++ R/DIscBIO-generic-plotSilhouette.R
@@ -1,23 +1,31 @@
 #' @title title
 #' @export
 #' @rdname plotSilhouette
+#' @param object object
 #' @param K K
-setGeneric("plotSilhouette", function(object,K) standardGeneric("plotSilhouette"))
+setGeneric(
+  name = "plotSilhouette",
+  def = function(object,K) standardGeneric("plotSilhouette")
+)
 #' @title title
 #' @description description
-#' @param object object
 #' @importFrom cluster silhouette
 #' @rdname plotSilhouette
 #' @export
-setMethod("plotSilhouette",
-          signature = "PSCANseq",
-          definition = function(object){
-            if ( length(object@kmeans$kpart) == 0 ) stop("run clustexp before plotsilhouette")
-            if ( length(unique(object@kmeans$kpart)) < 2 ) stop("only a single cluster: no silhouette plot")
-            col=c("black","blue","green","red","yellow","gray")
+setMethod(
+  f = "plotSilhouette",
+  signature = "PSCANseq",
+  definition = function(object, K) {
+    if (length(object@kmeans$kpart) == 0) {
+      stop("run clustexp before plotsilhouette")
+    }
+    if (length(unique(object@kmeans$kpart)) < 2) {
+      stop("only a single cluster: no silhouette plot")
+    }
+    col <- c("black", "blue", "green", "red", "yellow", "gray")
 		kpart <- object@kmeans$kpart
-            distances  <- dist.gen(object@distances)
-            si <- silhouette(kpart,distances)
-            plot(si,col=col[1:K])
-          }
-          )
+    distances <- dist.gen(object@distances)
+    si <- silhouette(kpart,distances)
+    plot(si,col=col[1:K])
+  }
+)
