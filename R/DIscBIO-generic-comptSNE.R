bfabfb8fc7a24848640559601b9e7868abae501f
Waldir Leoncio <waldir.leoncio@gmail.com>
2019-12-18 15:42:02 +0100

    Added quiet argument

--- R/DIscBIO-generic-comptSNE.R
+++ R/DIscBIO-generic-comptSNE.R
@@ -1,23 +1,34 @@
 #' @title title
 #' @export
 #' @rdname comptSNE
-setGeneric("comptSNE", function(object,rseed=15555) standardGeneric("comptSNE"))
+setGeneric(
+  name = "comptSNE",
+  def = function(object, rseed = 15555, quiet = FALSE) {
+    standardGeneric("comptSNE")
+  }
+)
 
 #' @title title
 #' @description description
 #' @param object object
 #' @param rseed rseed
+#' @param quiet if `TRUE`, suppresses intermediate output
 #' @importFrom tsne tsne
 #' @rdname comptSNE
 #' @export
-setMethod("comptSNE",
-          signature = "PSCANseq",
-          definition = function(object,rseed){
-            if ( length(object@kmeans$kpart) == 0 ) stop("run clustexp before comptsne")
-            set.seed(rseed)
-            di <- dist.gen(as.matrix(object@distances))
-            ts <- tsne(di,k=2)
-            object@tsne <- as.data.frame(ts)
-            return(object)
-          }
-          )
+setMethod(
+  "comptSNE",
+  signature = "PSCANseq",
+  definition = function(object, rseed, quiet = FALSE) {
+    if (length(object@kmeans$kpart) == 0) stop("run clustexp before comptsne")
+    set.seed(rseed)
+    di <- dist.gen(as.matrix(object@distances))
+    if (quiet) {
+      ts <- suppressMessages(tsne(di, k = 2))
+    } else {
+      ts <- tsne(di, k = 2)
+    }
+      object@tsne <- as.data.frame(ts)
+      return(object)
+  }
+)
