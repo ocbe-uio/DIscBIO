48b186f00a430fafd5c21909e87dac452aedba6a
Waldir Leoncio <waldir.leoncio@gmail.com>
2019-12-18 17:26:25 +0100

    Improved formatting

--- R/Jaccard.R
+++ R/Jaccard.R
@@ -10,7 +10,9 @@
 #' @importFrom graphics barplot box
 Jaccard <- function(object, Clustering, K, plot = TRUE) {
     JACCARD <-c()
-    if ( ! ( Clustering %in% c( "K-means","MB") ) ) stop("Clustering has to be either K-means or MB")
+    if (!(Clustering %in% c( "K-means","MB"))) {
+        stop("Clustering has to be either K-means or MB")
+    }
     JS <- function(data, indices) {
       d <- data[indices, ] # allows boot to select sample 
       jac <- suppressMessages(distance(t(d), method = "jaccard"))
@@ -18,21 +20,39 @@ Jaccard <- function(object, Clustering, K, plot = TRUE) {
       JSmean <- mean(jac1)
       return(JSmean)
     }
-    for (i in 1:K){
-        if (Clustering=="K-means"){
-            results <- boot(data=object@fdata[,which(object@kmeans$kpart==i)], statistic=JS, R=100,stype = "f")
-            JACCARD[i]<-round(mean(results$t),digits=3)            # to get the mean of all bootstrappings (mean of mean Jaccard values)
-    
+    for (i in 1:K) {
+        # Optimize by avoiding if every loop. Only thing variable is data
+        if (Clustering == "K-means") {
+            results <- boot(
+                data = object@fdata[, which(object@kmeans$kpart == i)],
+                statistic = JS,
+                R = 100,
+                stype = "f"
+            )
+            # to get the mean of all bootstrappings (mean of mean Jaccard values)
+            JACCARD[i] <- round(mean(results$t), digits = 3)
         }
-        if (Clustering=="MB"){
-            results <- boot(data=object@fdata[,which(object@MBclusters$clusterid==i)], statistic=JS, R=100,stype = "f")
-            JACCARD[i]<-round(mean(results$t),digits=3)            # to get the mean of all bootstrappings (mean of mean Jaccard values)
-    
+        if (Clustering == "MB") {
+            results <- boot(
+                data = object@fdata[, which(object@MBclusters$clusterid == i)],
+                statistic = JS,
+                R = 100,
+                stype = "f"
+            )
+            # to get the mean of all bootstrappings (mean of mean Jaccard values)
+            JACCARD[i] <- round(mean(results$t), digits = 3)
         }
     }
     if (plot) {
-        barplot(JACCARD,names.arg=1:length(JACCARD),ylab="Mean Jaccard's similarity values",xlab="Clusters",
-        las=1,ylim=c(0,1),col=c("black","blue","green","red","yellow","gray"))
+        barplot(
+            height = JACCARD,
+            names.arg = 1:length(JACCARD),
+            ylab = "Mean Jaccard's similarity values",
+            xlab = "Clusters",
+            las = 1,
+            ylim = c(0,1),
+            col = c("black", "blue", "green", "red", "yellow", "gray")
+        )
         box()
     }
     return(JACCARD)
