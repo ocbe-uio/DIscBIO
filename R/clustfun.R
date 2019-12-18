cd2d970d5084733ed7746833bb81cdbcd4df4afc
Waldir Leoncio <waldir.leoncio@gmail.com>
2019-12-18 15:02:37 +0100

    Added quiet option

--- R/clustfun.R
+++ R/clustfun.R
@@ -11,18 +11,21 @@
 #' @param B.gap B.gap
 #' @param cln cln
 #' @param rseed rseed
+#' @param quiet if `TRUE`, intermediate output is suppressed
 #' @importFrom cluster clusGap maxSE
 #' @importFrom stats kmeans
 #' @importFrom fpc clusterboot
-clustfun <- function(x,clustnr=20,bootnr=50,metric="pearson",do.gap=TRUE,SE.method="Tibs2001SEmax",SE.factor=.25,B.gap=50,cln=0,rseed=17000)
-{
+clustfun <- function(
+  x,clustnr=20,bootnr=50,metric="pearson",do.gap=TRUE,SE.method="Tibs2001SEmax",
+  SE.factor=.25,B.gap=50,cln=0,rseed=17000, quiet = FALSE
+) {
   if ( clustnr < 2) stop("Choose clustnr > 1")
   di <- dist.gen(t(x),method=metric)
   if ( do.gap | cln > 0 ){
     gpr <- NULL
     if ( do.gap ){
       set.seed(rseed)
-      gpr <- clusGap(as.matrix(di), FUNcluster = kmeans, K.max = clustnr, B = B.gap) 
+      gpr <- clusGap(as.matrix(di), FUNcluster = kmeans, K.max = clustnr, B = B.gap, verbose = !quiet) 
       if ( cln == 0 ) cln <- maxSE(gpr$Tab[,3],gpr$Tab[,4],method=SE.method,SE.factor)
     }    
     if ( cln <= 1 ) {
@@ -30,7 +33,7 @@ clustfun <- function(x,clustnr=20,bootnr=50,metric="pearson",do.gap=TRUE,SE.meth
       names(clb$result$partition) <- names(x)
       return(list(x=x,clb=clb,gpr=gpr,di=di))
     }
-    clb <- clusterboot(di,B=bootnr,distances=FALSE,bootmethod="boot",clustermethod=KmeansCBI,krange=cln,scaling=FALSE,multipleboot=FALSE,bscompare=TRUE,seed=rseed)
+    clb <- clusterboot(di,B=bootnr,distances=FALSE,bootmethod="boot",clustermethod=KmeansCBI,krange=cln,scaling=FALSE,multipleboot=FALSE,bscompare=TRUE,seed=rseed, count = !quiet)
     return(list(x=x,clb=clb,gpr=gpr,di=di))
   }
 }
