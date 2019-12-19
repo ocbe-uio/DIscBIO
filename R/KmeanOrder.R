a7b97e4fdc957523319a27c6b8c270b2729554f9
Waldir Leoncio <waldir.leoncio@gmail.com>
2019-12-19 12:58:10 +0100

    Resolved scope error

--- R/KmeanOrder.R
+++ R/KmeanOrder.R
@@ -7,20 +7,27 @@
 #' @param quiet if `TRUE`, suppresses intermediary output
 #' @param export if `TRUE`, exports order table to csv
 #' @importFrom TSCAN TSCANorder
-KmeanOrder<-function(object,Clusters,sampleNames, quiet = FALSE, export = TRUE){
-	lpsmclust <- Exprmclust(object,clusternum =4,reduce = F, cluster = Clusters)
+KmeanOrder <- function(
+	object, Clusters, sampleNames, quiet = FALSE, export = TRUE
+) {
+	lpsmclust <- Exprmclust(
+		object, clusternum = 4, reduce = F, cluster = Clusters
+	)
 	lpsorder <- TSCANorder(lpsmclust)
-	orderID<-lpsorder
-	order<-c(1:length(lpsorder))
-	orderTable<-data.frame(order,orderID)
+	orderID <- lpsorder
+	order <- c(1:length(lpsorder))
+	orderTable <- data.frame(order, orderID)
 	if (export) {
-		write.csv(orderTable, file = "Cellular_pseudo-time_ordering_based_on_k-meansc-lusters.csv")
+		write.csv(
+			orderTable,
+			file = "Cellular_pseudo-time_ordering_based_on_k-meansc-lusters.csv"
+		)
 	}
 	if (!quiet) {
 		print(orderTable)
 	}
-	FinalOrder<-orderTable[match(sampleNames, orderTable$orderID),]
-	sc@kordering<-FinalOrder[,1]
-	names(sc@kordering)<-names(Clusters)
-	return(sc@kordering)
+	FinalOrder <- orderTable[match(sampleNames, orderTable$orderID), ]
+	out_order <- FinalOrder[, 1]
+	names(out_order) <- names(Clusters)
+	return(out_order)
 }
