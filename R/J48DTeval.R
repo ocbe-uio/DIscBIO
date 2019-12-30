3eed68a43477cef2474feac136758f789add546c
Waldir Leoncio <waldir.leoncio@gmail.com>
2019-12-30 16:01:47 +0100

    Incorported SN, SP, ACC and MCC into parent functions

--- R/J48DTeval.R
+++ R/J48DTeval.R
@@ -6,8 +6,12 @@
 #' @param First A string vector showing the first target cluster.  Default is "CL1"
 #' @param Second A string vector showing the second target cluster.  Default is "CL2"
 
+J48DTeval<- function(data,num.folds=10,First="CL1",Second="CL2"){
+	exp.imput.df<-as.data.frame(t(data))
 	num.instances<-nrow(exp.imput.df)
 	indices<-1:num.instances
+	classVector<- factor(colnames(data))
+
 	cross.val <- function(exp.df, class.vec, segments, performance, class.algo){
 		#Start cross validation loop
 		class1 <- levels(class.vec)[1]
@@ -52,7 +56,7 @@
 		return(performance)
 	}
 	
-
+	
 	cv.segments<-split(sample(indices),rep(1:num.folds,length=num.instances))
 	j48.performance<-c("TP"=0,"FN"=0,"FP"=0,"TN"=0)
 	j48.performance<-cross.val(exp.imput.df,classVector,cv.segments,j48.performance,"J48")
@@ -62,7 +66,33 @@
 	rownames(j48.confusion.matrix)<-c(paste0("Predicted",First), paste0("Predicted",Second))
 	colnames(j48.confusion.matrix)<-c(First,Second)
 	print(j48.confusion.matrix)
-
+	
+	SN <- function(con.mat){
+		TP <- con.mat[1,1]
+		FN <- con.mat[2,1]
+		return(TP/(TP+FN))
+	}
+	SP <- function(con.mat){
+		TN <- con.mat[2,2]
+		FP <- con.mat[1,2]
+		return(TN/(TN+FP))
+	}
+	ACC <- function(con.mat){
+		TP <- con.mat[1,1]
+		FN <- con.mat[2,1]
+		TN <- con.mat[2,2]
+		FP <- con.mat[1,2]
+		return((TP+TN)/(TP+FN+TN+FP))
+	}
+	MCC <- function(con.mat){
+		TP <- con.mat[1,1]
+		FN <- con.mat[2,1]
+		TN <- con.mat[2,2]
+		FP <- con.mat[1,2]
+		denom <- sqrt((TP+FP)*(TP+FN)*(TN+FP)*(TN+FN))
+		denom <- ifelse(denom==0, NA, denom)
+		return((TP*TN-FP*FN)/denom)
+	}
 	j48.sn<-SN(j48.confusion.matrix)
 	j48.sp<-SP(j48.confusion.matrix)
 	j48.acc<-ACC(j48.confusion.matrix)
--- R/RpartEVAL.R
+++ R/RpartEVAL.R
@@ -6,8 +6,12 @@
 #' @param First A string vector showing the first target cluster.  Default is "CL1"
 #' @param Second A string vector showing the second target cluster.  Default is "CL2"
 
+
+RpartEVAL<- function(data,num.folds=10,First="CL1",Second="CL2"){
+	exp.imput.df<-as.data.frame(t(data))
 	num.instances<-nrow(exp.imput.df)
 	indices<-1:num.instances
+	classVector<- factor(colnames(data))
 	
 	cross.val <- function(exp.df, class.vec, segments, performance, class.algo){
 		#Start cross validation loop
@@ -52,7 +56,7 @@
 		}
 		return(performance)
 	}
-
+	
 	cv.segments<-split(sample(indices),rep(1:num.folds,length=num.instances))
 	Rpart.performance<-c("TP"=0,"FN"=0,"FP"=0,"TN"=0)
 	Rpart.performance<-cross.val(exp.imput.df,classVector,cv.segments,Rpart.performance,"rpart")
@@ -62,6 +66,33 @@
 	colnames(Rpart.confusion.matrix)<-c(First,Second)
 	print(Rpart.confusion.matrix)
 
+	SN <- function(con.mat){
+		TP <- con.mat[1,1]
+		FN <- con.mat[2,1]
+		return(TP/(TP+FN))
+	}
+	SP <- function(con.mat){
+		TN <- con.mat[2,2]
+		FP <- con.mat[1,2]
+		return(TN/(TN+FP))
+	}
+	ACC <- function(con.mat){
+		TP <- con.mat[1,1]
+		FN <- con.mat[2,1]
+		TN <- con.mat[2,2]
+		FP <- con.mat[1,2]
+		return((TP+TN)/(TP+FN+TN+FP))
+	}
+	MCC <- function(con.mat){
+		TP <- con.mat[1,1]
+		FN <- con.mat[2,1]
+		TN <- con.mat[2,2]
+		FP <- con.mat[1,2]
+		denom <- sqrt((TP+FP)*(TP+FN)*(TN+FP)*(TN+FN))
+		denom <- ifelse(denom==0, NA, denom)
+		return((TP*TN-FP*FN)/denom)
+	}
+	
 	Rpart.sn<-SN(Rpart.confusion.matrix)
 	Rpart.sp<-SP(Rpart.confusion.matrix)
 	Rpart.acc<-ACC(Rpart.confusion.matrix)
