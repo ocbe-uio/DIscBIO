9b1f606eab790ea0e84bd70fb154f966b9c3f97f
Salim Ghannoum <36110882+SystemsBiologist@users.noreply.github.com>
2020-02-06 17:38:14 +0100

    Adding a demo dataset "valuesG1msReduced.rda"

--- R/datasets.R
+++ R/datasets.R
@@ -1,6 +1,7 @@
 #' valuesG1ms A dataset of 94 single cells from a myxoid liposarcoma cell line. 
 #' Columns refer to samples and rows refer to genes. The last 92 rows refer to external RNA controls consortium (ERCC) spike-ins 
 #'
+#' valuesG1msReduced A dataset of 30 single cells from a myxoid liposarcoma cell line. 
 #' @name valuesG1ms
 #' @docType data
 NULL
