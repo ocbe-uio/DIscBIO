f5ef327d54bcd8f2b11743af6ff3b4aede48c73c
Waldir Leoncio <waldir.leoncio@gmail.com>
2020-02-06 10:12:34 +0100

    Imported AnnotationDbi::keys

--- R/DIscBIO-generic-DEGanalysis2clust.R
+++ R/DIscBIO-generic-DEGanalysis2clust.R
@@ -13,6 +13,7 @@
 #' @importFrom samr samr samr.compute.delta.table samr.plot samr.compute.siggenes.table
 #' @importFrom graphics title
 #' @importFrom utils write.csv capture.output
+#' @importFrom AnnotationDbi keys
 #' @examples
 #' \dontrun{
 #' sc <- DISCBIO(valuesG1ms)
