f31d8a80a8a72080e34449e0ec89557c8b6b4229
Waldir Leoncio <waldir.leoncio@gmail.com>
2020-02-07 16:53:26 +0100

    Improved formatting

--- R/DIscBIO-generic-plotGap.R
+++ R/DIscBIO-generic-plotGap.R
@@ -7,22 +7,25 @@
 #' sc <- DISCBIO(valuesG1msReduced) # changes signature of data
 #' sc <- Clustexp(sc, cln=3) # data must be clustered before plotting
 #' plotGap(sc)
-setGeneric("plotGap", function(object, y_limits = NULL) standardGeneric("plotGap"))
+setGeneric(
+  "plotGap", function(object, y_limits = NULL) standardGeneric("plotGap")
+)
 
 #' @rdname plotGap
-setMethod("plotGap",
-          signature = "DISCBIO",
-          definition = function(object, y_limits){
-            if ( length(object@kmeans$kpart) == 0 ) stop("run clustexp before plotgap")
-            if (is.null(y_limits)) {
-              y_lo <- min(x$Tab[, "gap"]) - 1 * max(x$Tab[, "SE.sim"])
-              y_up <- max(x$Tab[, "gap"]) + 1 * max(x$Tab[, "SE.sim"])
-              y_limits <- c(y_lo, y_up)
-            } else {
-            }
-            plot(
-              object@kmeans$gap, las=1, ylim = y_limits,
-              main="Gap Statistics"
-            )
-          }
-          )		 
+setMethod(
+	f = "plotGap",
+	signature = "DISCBIO",
+	definition = function(object, y_limits) {
+		if (length(object@kmeans$kpart) == 0) {
+			stop("run clustexp before plotgap")
+		}
+		if (is.null(y_limits)) {
+			y_lo <- min(x$Tab[, "gap"]) - 1 * max(x$Tab[, "SE.sim"])
+			y_up <- max(x$Tab[, "gap"]) + 1 * max(x$Tab[, "SE.sim"])
+			y_limits <- c(y_lo, y_up)
+		}
+		plot(
+			object@kmeans$gap, las=1, ylim = y_limits, main="Gap Statistics"
+		)
+	}
+)		 
