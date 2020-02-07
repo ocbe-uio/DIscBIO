8df8775e6600112226fae0091caf7d29ffa424bb
Waldir Leoncio <waldir.leoncio@gmail.com>
2020-02-07 09:38:00 +0100

    Documentation fixes

--- R/datasets.R
+++ R/datasets.R
@@ -1,7 +1,15 @@
 #' valuesG1ms A dataset of 94 single cells from a myxoid liposarcoma cell line. 
-#' Columns refer to samples and rows refer to genes. The last 92 rows refer to external RNA controls consortium (ERCC) spike-ins 
+#' Columns refer to samples and rows refer to genes. The last 92 rows refer to external RNA controls consortium (ERCC) spike-ins.
 #'
 #' valuesG1msReduced A dataset of 30 single cells from a myxoid liposarcoma cell line. 
 #' @name valuesG1ms
+#' @docType data
+NULL
+
+#' valuesG1msReduced A dataset of 30 single cells from a myxoid liposarcoma cell line.
+#' Columns refer to samples and rows refer to genes. The last 92 rows refer to external RNA controls consortium (ERCC) spike-ins.
+#'
+#' 
+#' @name valuesG1msReduced
 #' @docType data
 NULL
