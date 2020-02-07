a4c0d054fedabacc1317b0551aba038dfa24ab22
Waldir Leoncio <waldir.leoncio@gmail.com>
2020-02-07 10:32:05 +0100

    Ignoring full dataset

--- R/datasets.R
+++ R/datasets.R
@@ -1,15 +1,6 @@
-#' valuesG1ms A dataset of 94 single cells from a myxoid liposarcoma cell line. 
-#' Columns refer to samples and rows refer to genes. The last 92 rows refer to external RNA controls consortium (ERCC) spike-ins.
-#'
-#' valuesG1msReduced A dataset of 30 single cells from a myxoid liposarcoma cell line. 
-#' @name valuesG1ms
-#' @docType data
-NULL
-
-#' valuesG1msReduced A dataset of 30 single cells from a myxoid liposarcoma cell line.
-#' Columns refer to samples and rows refer to genes. The last 92 rows refer to external RNA controls consortium (ERCC) spike-ins.
-#'
+#' @title Single-cells data from a myxoid liposarcoma cell line
 #' 
+#' @description A dataset of 30 single cells from a myxoid liposarcoma cell line. Columns refer to samples and rows refer to genes. The last 92 rows refer to external RNA controls consortium (ERCC) spike-ins. This dataset is part of a larger dataset containing 94 single cells. The complete dataset is fully compatible with this package and an rda file can be obtained on https://github.com/ocbe-uio/DIscBIO/blob/dev/data/valuesG1ms.rda 
 #' @name valuesG1msReduced
 #' @docType data
 NULL
