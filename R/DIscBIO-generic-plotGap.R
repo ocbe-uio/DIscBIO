a473ef53791649c75b0a0ac353d5d73bae21973c
Waldir Leoncio <waldir.leoncio@gmail.com>
2020-02-07 16:51:48 +0100

    Added argument to plotGap to config plot limits

--- R/DIscBIO-generic-plotGap.R
+++ R/DIscBIO-generic-plotGap.R
@@ -2,17 +2,27 @@
 #' @export
 #' @rdname plotGap
 #' @param object \code{DISCBIO} class object.
+#' @param y_limits 2-length numeric vector with the limits for the gap plot
 #' @examples
 #' sc <- DISCBIO(valuesG1msReduced) # changes signature of data
 #' sc <- Clustexp(sc, cln=3) # data must be clustered before plotting
 #' plotGap(sc)
-setGeneric("plotGap", function(object) standardGeneric("plotGap"))
+setGeneric("plotGap", function(object, y_limits = NULL) standardGeneric("plotGap"))
 
 #' @rdname plotGap
 setMethod("plotGap",
           signature = "DISCBIO",
-          definition = function(object){
+          definition = function(object, y_limits){
             if ( length(object@kmeans$kpart) == 0 ) stop("run clustexp before plotgap")
-            plot(object@kmeans$gap,ylim=c(0.1,0.5),las=1,main="Gap Statistics")
+            if (is.null(y_limits)) {
+              y_lo <- min(x$Tab[, "gap"]) - 1 * max(x$Tab[, "SE.sim"])
+              y_up <- max(x$Tab[, "gap"]) + 1 * max(x$Tab[, "SE.sim"])
+              y_limits <- c(y_lo, y_up)
+            } else {
+            }
+            plot(
+              object@kmeans$gap, las=1, ylim = y_limits,
+              main="Gap Statistics"
+            )
           }
           )		 
