0462d78da128a49e44af2c42415309c5cfc5c91d
Waldir Leoncio <waldir.leoncio@gmail.com>
2020-02-10 14:17:14 +0100

    Adjusted test units to reduced dataset

--- tests/testthat/test.DIscBIO.IMP.R
+++ tests/testthat/test.DIscBIO.IMP.R
@@ -4,11 +4,11 @@
 
 context("Data loading and pre-processing")
 
-# The "valuesG1ms" is the only needed dataset
-sc <- DISCBIO(valuesG1ms)
+sc <- DISCBIO(valuesG1msReduced)  # Reduced dataset used for testing
 
 test_that("Loading datasets generate the expected output", {
-    expect_equal(dim(valuesG1ms), c(59838, 94))
+    expect_equal(dim(valuesG1ms), c(59838, 94)) # OK to remove if broken
+    expect_equal(dim(valuesG1msReduced), c(1092, 30))
 })
 
 test_that("Data signature changes", {
@@ -19,7 +19,7 @@ test_that("Data signature changes", {
 # This function will be used only if the dataset has ERCC
 sc <- NoiseFiltering(sc, plot=FALSE, export=FALSE, quiet=TRUE)
 test_that("Noise filtering is added", {
-    expect_equal(length(sc@noiseF), 5684)
+    expect_equal(length(sc@noiseF), 341)
 })
 
 # In this case this function is used to normalize the reads
@@ -30,14 +30,14 @@ sc <- Normalizedata(
 
 test_that("Data is normalized", {
     expect_equal(class(sc@fdata), "data.frame")
-    expect_output(str(sc@fdata), "59746 obs. of  94 variables")
+    expect_output(str(sc@fdata), "1000 obs. of  30 variables")
 })
 
 # This function can be used for: 1- filtering and normalizing the dataset that has no ERCC. 2- to normalize and filter genes and cells after the noise filtering.
 sc <- FinalPreprocessing(sc, GeneFlitering="NoiseF", export=FALSE, quiet=TRUE)
 
 test_that("Data is normalized", {
-    expect_equal(dim(sc@fdata), c(5684, 94))
+    expect_equal(dim(sc@fdata), c(341, 30))
 })
 
 # ---------------------------------------------------------------------------- #
@@ -46,22 +46,23 @@ test_that("Data is normalized", {
 
 context("K-means clustering")
 
-sc <- Clustexp(sc, cln=3, quiet=TRUE) # K-means clustering
+sc <- Clustexp(sc, cln=2, quiet=TRUE) # K-means clustering
 sc <- comptSNE(sc, rseed=15555, quiet=TRUE)
 
 test_that("tSNE is computed", {
     expect_equal(class(sc@tsne), "data.frame")
-    expect_output(str(sc@tsne), "94 obs. of  2 variables")
+    expect_output(str(sc@tsne), "30 obs. of  2 variables")
 })
 
 test_that("Cluster plots output is as expexted", {
-    expect_equal(
-        object = Jaccard(sc, Clustering="K-means", K=1, plot = FALSE),
-        expected = .68
+    expect_equivalent(
+        object = Jaccard(sc, Clustering="K-means", K=2, plot = FALSE),
+        expected = c(.790, .653),
+        tolerance = .01
     )
     expect_equal(
         object = KMclustheatmap(sc, hmethod = "single", plot = FALSE),
-        expected = c(3, 1, 2)
+        expected = c(1, 2)
     )
 })
 
@@ -70,36 +71,26 @@ test_that("Cluster plots output is as expexted", {
 context("Outliers")
 
 Outliers <- FindOutliersKM(
-    sc, K=3, outminc=5, outlg=2, probthr=.5*1e-3, thr=2**-(1:40),
+    sc, K=2, outminc=5, outlg=2, probthr=.5*1e-3, thr=2**-(1:40),
     outdistquant=.75, plot = FALSE, quiet = TRUE
 )
 
 # Adjusting outliers
 outlg <- round(length(sc@fdata[, 1]) / 200) # The cell will be considered as an outlier if it has a minimum of 0.5% of the number of filtered genes as outlier genes.
 Outliers2 <- FindOutliersKM(
-    sc, K=3, outminc=5, outlg=outlg, probthr=.5*1e-3, thr=2**-(1:40),
+    sc, K=2, outminc=5, outlg=outlg, probthr=.5*1e-3, thr=2**-(1:40),
     outdistquant=.75, plot = FALSE, quiet = TRUE
 )
 Order <- KmeanOrder(sc, quiet = TRUE, export = FALSE)
 
 test_that("Outliers are the expected", {
-    expect_equivalent(
-        object = Outliers,
-        expected = c(
-            1, 3:9, 11:15, 17:25, 27, 29, 35, 37, 40, 43, 45, 52, 58, 61:63, 66,
-            71:75, 78:79, 82, 85, 92
-        )
-    )
-    expect_equivalent(Outliers2, c(1, 13, 22))
+    expect_equivalent(Outliers, c(3, 7, 19))
+    expect_equivalent(Outliers2, c(3, 7, 19))
     expect_equivalent(
         object = Order@kordering, 
         expected = c(
-            23, 72, 29, 11, 19, 26, 48, 77, 80, 83, 82, 16, 88, 5, 13, 24, 86, 
-            42, 34, 45, 87, 15, 65, 27, 10, 58, 89, 35, 71, 62, 32, 44, 38, 56, 
-            55, 50, 53, 2, 49, 59, 54, 61, 60, 1, 43, 9, 31, 6, 18, 64, 46, 41, 
-            22, 20, 33, 37, 63, 90, 52, 70, 66, 92, 39, 51, 30, 93, 12, 14, 75, 
-            78, 47, 28, 91, 68, 21, 73, 94, 25, 17, 8, 74, 57, 69, 4, 84, 7, 40,
-            81, 85, 79, 67, 3, 76, 36
+            23, 20, 6, 21, 27, 26, 24, 28, 10, 19, 15, 25, 16, 8, 14, 13, 22, 4,
+            17, 2, 3, 18, 11, 29, 9, 5, 12, 1, 30, 7
         )
     )
 })
@@ -119,7 +110,7 @@ cdiff2 <- DEGanalysis(
 
 # differential expression analysis between two particular clusters.
 cdiff3 <- DEGanalysis2clust(
-    sc, Clustering="K-means", K=3, fdr=.15, name="Name", First="CL1",
+    sc, Clustering="K-means", K=2, fdr=.15, name="Name", First="CL1",
     Second="CL2", export=FALSE, quiet=TRUE, plot=FALSE
 )
 
@@ -133,7 +124,7 @@ test_that("DEGs are calculated", {
 sigDEG <- cdiff3[[1]]
 
 DATAforDT <- ClassVectoringDT(
-    sc, Clustering="K-means", K=3, First="CL1", Second="CL2", sigDEG,
+    sc, Clustering="K-means", K=2, First="CL1", Second="CL2", sigDEG,
     quiet = TRUE
 )
 
@@ -147,12 +138,12 @@ rpartEVAL <- RpartEVAL(
 )
 
 test_that("Decision tree elements are defined", {
-    expect_output(str(DATAforDT), "5 obs. of  79 variables")
+    expect_output(str(DATAforDT), "3 obs. of  30 variables")
     expect_s3_class(j48dt, "J48")
     expect_s3_class(summary(j48dt), "Weka_classifier_evaluation")
-    expect_identical(j48dt_eval, c(TP = 18, FN = 11, FP = 6, TN = 44))
+    expect_identical(j48dt_eval, c(TP = 14, FN = 4, FP = 5, TN = 7))
     expect_s3_class(rpartDT, "rpart")
-    expect_identical(rpartEVAL, c(TP = 19, FN = 10, FP = 7, TN = 43))
+    expect_identical(rpartEVAL, c(TP = 15, FN = 3, FP = 3, TN = 9))
 })
 
 # ---------------------------------------------------------------------------- #
@@ -163,7 +154,7 @@ context("Model-based clustering")
 
 # Technically, this should be done before Clustexp, but it's ok in practice to 
 # apply it after K-means because it uses different slots.
-sc <- Exprmclust(sc, quiet = TRUE)
+sc <- Exprmclust(sc, K=2, quiet=TRUE)
 
 test_that("Model-based clustering elements are OK", {
     expect_identical(
@@ -175,40 +166,34 @@ test_that("Model-based clustering elements are OK", {
 sc <- comptsneMB(sc, rseed=15555, quiet = TRUE)
 
 test_that("tSNE clustering works fine", {
-    expect_equal(dim(sc@MBtsne), c(94, 2))
+    expect_equal(dim(sc@MBtsne), c(30, 2))
 })
 
 # --------------------------------- Outliers --------------------------------- #
 
 context("MB outliers")
 
-sc <- Clustexp(sc, cln=3, quiet=TRUE)
+sc <- Clustexp(sc, cln=2, quiet=TRUE)
 
 Outliers <- FindOutliersMB(
-    sc, K=3, outminc=5, outlg=2, probthr=.5*1e-3, thr=2**-(1:40),
+    sc, K=2, outminc=5, outlg=2, probthr=.5*1e-3, thr=2**-(1:40),
     outdistquant=.75, plot = FALSE, quiet = TRUE
 )
 outlg <- round(length(sc@fdata[, 1]) / 200) # The cell will be considered as an outlier if it has a minimum of 0.5% of the number of filtered genes as outlier genes. 
 Outliers2 <- FindOutliersMB(
-    sc, K=3, outminc=5, outlg=outlg, probthr=.5*1e-3, thr=2**-(1:40),
+    sc, K=2, outminc=5, outlg=outlg, probthr=.5*1e-3, thr=2**-(1:40),
     outdistquant=.75, plot = FALSE, quiet = TRUE
 )
 
 test_that("MB clustering and outliers work as expected", {
-    expect_equal(
-        object = Jaccard(sc, Clustering="MB", K=1, plot = FALSE),
-        expected = .66
-    )
     expect_equivalent(
-        object = Outliers,
-        expected = c(
-            1, 3, 4, 5, 7, 9, 11, 12, 13, 14, 15, 16, 17, 18, 19, 21, 22, 23, 
-            24, 25, 27, 29, 35, 40, 43, 49, 54, 58, 61, 63, 68, 72, 73, 75, 78, 
-            79, 82, 85, 94
-        )
+        object = Jaccard(sc, Clustering="MB", K=2, plot = FALSE),
+        expected = c(.819, .499),
+        tolerance = 0.01
     )
-    expect_equal(outlg, 28)
-    expect_equal(Outliers2, c("G1" = 1, "G1.12" = 13, "G1.21" = 22))
+    expect_equivalent(Outliers, c(3, 4, 7))
+    expect_equal(outlg, 2)
+    expect_equal(Outliers2, c("G1_12" = 3, "G1_18" = 4, "G1_21" = 7))
 })
 
 sc <- MB_Order(sc, quiet = TRUE, export = FALSE)
@@ -218,15 +203,11 @@ test_that("More MB things are OK", {
     expect_equal(
         object = sc@MBordering,
         expected = c(
-            39, 29, 56, 35, 25, 52, 54, 23, 37, 26, 48, 46, 40, 38, 49, 33, 18, 
-            44, 47, 70, 32, 36, 57, 42, 53, 6, 27, 71, 41, 34, 88, 64, 94, 86, 
-            76, 72, 51, 59, 68, 60, 84, 78, 89, 79, 61, 63, 81, 58, 92, 80, 93, 
-            85, 83, 62, 69, 77, 67, 75, 90, 82, 65, 73, 19, 17, 50, 91, 7, 11, 
-            15, 12, 28, 66, 20, 87, 21, 4, 3, 45, 43, 2, 8, 5, 1, 55, 9, 10, 24,
-            14, 74, 16, 13, 22, 30, 31
+            8, 28, 27, 18, 21, 10, 29, 26, 17, 25, 5, 13, 12, 19, 16, 15, 23, 
+            20, 30, 14, 7, 9, 24, 22, 3, 2, 4, 6, 1, 11
         )
     )
-    expect_equal(mb_heat, c(2, 1, 3))
+    expect_equal(mb_heat, c(1, 2))
 })
 
 # ----------------------------------- DEGs ----------------------------------- #
@@ -234,7 +215,7 @@ test_that("More MB things are OK", {
 context("MB DEGs")
 
 # Binomial DE analysis
-cdiff1 <- MBClustDiffGenes(sc, K=1, fdr=.2, export=FALSE, quiet=TRUE)
+cdiff1 <- MBClustDiffGenes(sc, K=2, fdr=.2, export=FALSE, quiet=TRUE)
 
 # DE analysis between all clusters
 cdiff2 <- DEGanalysis(
@@ -257,7 +238,7 @@ test_that("DEGs are calculated", {
 # Decision tree   
 sigDEG <- cdiff3[[1]]
 DATAforDT <- ClassVectoringDT(
-    sc, Clustering="MB", K=3, First="CL1", Second="CL2", sigDEG, quiet = TRUE
+    sc, Clustering="MB", K=2, First="CL1", Second="CL2", sigDEG, quiet = TRUE
 )
 j48dt <- J48DT(DATAforDT, quiet = TRUE, plot = FALSE)
 j48dt_eval <- J48DTeval(
@@ -269,10 +250,10 @@ rpartEVAL <- RpartEVAL(
 )
 
 test_that("Decision tree elements are defined", {
-    expect_output(str(DATAforDT), "340 obs. of  65 variables") # used to be 31
+    expect_output(str(DATAforDT), "23 obs. of  30 variables") # used to be 31
     expect_s3_class(j48dt, "J48")
     expect_s3_class(summary(j48dt), "Weka_classifier_evaluation")
-    expect_identical(j48dt_eval, c(TP = 40, FN = 8, FP = 10, TN = 7))
+    expect_identical(j48dt_eval, c(TP = 21, FN = 2, FP = 4, TN = 3))
     expect_s3_class(rpartDT, "rpart")
-    expect_identical(rpartEVAL, c(TP = 38, FN = 10, FP = 14, TN = 3))
+    expect_identical(rpartEVAL, c(TP = 19, FN = 4, FP = 4, TN = 3))
 })
