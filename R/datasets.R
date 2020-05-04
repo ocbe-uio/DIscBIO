4bbe9a8f72f85e6bf9a33a8703a9b2a1941d265d
Waldir Leoncio <waldir.leoncio@gmail.com>
2020-05-04 09:15:13 +0200

    Fixed text and package date

--- R/datasets.R
+++ R/datasets.R
@@ -4,20 +4,20 @@
 #'   line. Columns refer to samples and rows refer to genes. The last 92 rows
 #'   refer to external RNA controls consortium (ERCC) spike-ins. This dataset is
 #'   part of a larger dataset containing 94 single cells. The complete dataset
-#'   is fully compatible with this package and an rda file can be obtained on
+#'   is fully compatible with this package and an rda file can be obtained at
 #'   https://github.com/ocbe-uio/DIscBIO/blob/dev/data/valuesG1ms.rda
 #' @name valuesG1msReduced
 #' @docType data
 NULL
 
 #' Human and Mouse Gene Identifiers.
-#' 
+#'
 #' Data.frame including ENTREZID, SYMBOL, and ENSEMBL gene identifiers of human and mouse genes.
-#' 
-#' @source Data were imported, modified, and formatted from the Mus.musculus (ver 1.3.1) and 
+#'
+#' @source Data were imported, modified, and formatted from the Mus.musculus (ver 1.3.1) and
 #' the Homo.sapiens (ver 1.3.1) BioConductor libraries.
-#' 
-#' @examples 
+#'
+#' @examples
 #' data(HumanMouseGeneIds)
 #' print(HumanMouseGeneIds[1:6,])
 #' @name HumanMouseGeneIds
