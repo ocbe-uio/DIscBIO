84074a2b6ca95d69ae89ecbca90e8a358a568173
Waldir Leoncio <waldir.leoncio@gmail.com>
2020-05-08 06:27:18 +0200

    Refactoring of integration tests and Jaccard()

--- tests/testthat/test.integration.R
+++ tests/testthat/test.integration.R
@@ -2,17 +2,21 @@
 # Testing integration with SingleCellExperiment
 # ==============================================================================
 context("Converting other formats to DISCBIO")
-
+# ------------------------------------------------------------------------------
+# Setting up datasets
+# ------------------------------------------------------------------------------
 pmbc_seurat <- Seurat::pbmc_small
 pmbc_sce <- Seurat::as.SingleCellExperiment(pmbc_seurat)
 g1_sce <- SingleCellExperiment::SingleCellExperiment(
-    list(counts=as.matrix(valuesG1msReduced))
+	list(counts=as.matrix(valuesG1msReduced))
 )
-
+# ------------------------------------------------------------------------------
+# Performing unit tests
+# ------------------------------------------------------------------------------
 test_that("Pure text gets formatted as DISCBIO", {
-    expect_true(is(as.DISCBIO(pmbc_seurat), "DISCBIO"))
+	expect_s4_class(as.DISCBIO(pmbc_seurat), "DISCBIO")
 })
 test_that("SCE file gets formatted as DISCBIO", {
-    expect_true(is(as.DISCBIO(pmbc_sce), "DISCBIO"))
-    expect_true(is(as.DISCBIO(g1_sce), "DISCBIO"))
+	expect_s4_class(as.DISCBIO(pmbc_sce), "DISCBIO")
+	expect_s4_class(as.DISCBIO(g1_sce), "DISCBIO")
 })
