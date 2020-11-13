31b33f07edbb75ed090df8f697da39b1d3445524
Waldir Leoncio <waldir.leoncio@gmail.com>
2020-11-13 13:54:41 +0100

    Code refactoring

--- R/PPI.R
+++ R/PPI.R
@@ -12,13 +12,13 @@
 #' corresponding `data.frame` object in R or and R object containing that
 #' information.
 PPI <- function(data, FileName = NULL, species = "9606") {
-    repos <- retrieveURL(data, species, "tsv")
-    # Process API request content
-    repo_content <- content(repos)
-    write.table(repo_content, file = "data.csv", sep = ",")
-    results <- read.table(file = "data.csv", sep = ",")
-    if (!is.null(FileName)) {
-        write.csv(results, file = paste0("PPI-", FileName, ".csv"))
-    }
-    return(results)
+	repos <- retrieveURL(data, species, "tsv")
+	# Process API request content
+	repo_content <- content(repos)
+	write.table(repo_content, file = "data.csv", sep = ",")
+	results <- read.table(file = "data.csv", sep = ",")
+	if (!is.null(FileName)) {
+		write.csv(results, file = paste0("PPI-", FileName, ".csv"))
+	}
+	return(results)
 }
