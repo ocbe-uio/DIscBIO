a99ec144a8a07b3bf81c6380963b5b5f4d595638
Waldir Leoncio <waldir.leoncio@gmail.com>
2020-11-13 13:52:17 +0100

    Replaced retrieval code with internal function

--- R/PPI.R
+++ R/PPI.R
@@ -13,31 +13,7 @@
 #' corresponding `data.frame` object in R or and R object containing that
 #' information.
 PPI <- function(data, FileName = NULL, species = "9606") {
-    # Save base enpoint as variable
-    string_api_url <- "https://string-db.org/api/"
-    output_format <- "tsv" #"json", "tsv-no-header", "tsv", "xml"
-    method <- "network"
-    your_identifiers <- ""
-    optional_parameters <- ""
-    # Construct API request
-    genes <- data
-    repos <- GET(
-        url = paste0(
-            string_api_url,
-            output_format,
-            '/',
-            method,
-            '?identifiers=',
-            paste(as.character(data), collapse = "%0d"),
-            "&",
-            "species=",
-            species
-        )
-    )
-    message(
-        "Examine response components = ", status_code(repos), "\t",
-        "(200 means successful)", "\n"
-    )
+    repos <- retrieveURL(data, species, "tsv")
     # Process API request content
     repo_content <- content(repos)
     #results <- read_tsv(repo_content)
